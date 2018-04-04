afc47f70b44118eb3f9aba94e53f00b1cde284dd
Shel Orock <sdo243@nyu.edu>
2018-04-04 06:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function currentLine(katzDeliLine) {
   let i = 0;
   while(i < katzDeliLine.length) {
     line.push(` ${i + 1}. ${katzDeli[i]}`)
+    i++;
   }
   if(katzDeliLine.length > 0){
       return `The line is currently: ${line}`;
